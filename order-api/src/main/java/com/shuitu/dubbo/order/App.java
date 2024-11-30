be7401d2f9f1b86ed461346168614bc30759c5d2
jingx38 <jingx38@users.noreply.github.com>
2024-11-30 06:07:13 +0000

    First code commit.

---
+++ order-api/src/main/java/com/shuitu/dubbo/order/App.java
@@ -0,0 +1,12 @@
+package com.shuitu.dubbo.order;
+
+/**
+* @author È«ºã
+*/
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ order-api/src/main/java/com/shuitu/dubbo/order/IOrderServices.java
@@ -0,0 +1,13 @@
+package com.shuitu.dubbo.order;
+
+/**
+* @author 全恒
+*/
+public interface IOrderServices {
+
+    /*
+     * 涓嬪崟鎿嶄綔
+     */
+    DoOrderResponse doOrder(DoOrderRequest request);
+
+}
